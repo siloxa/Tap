94abb9f61dae2b90544584971564cd395c4c7f4a
Soroush <shuoros@yahoo.com>
2024-01-16 13:37:52 +0100

    IMPEMENT[Header scroll and downlod btns scroll]

---
+++ assets/js/main.js
@@ -0,0 +1,35 @@
+var lastId,
+ topMenu = $(".header-menu"),
+ topMenuHeight = topMenu.outerHeight()+1,
+ menuItems = topMenu.find("a"),
+ scrollItems = menuItems.map(function(){
+   var item = $($(this).attr("href"));
+    if (item.length) { return item; }
+ });
+
+$(window).scroll(function(){
+    var fromTop = $(this).scrollTop()+topMenuHeight;
+    var cur = scrollItems.map(function(){
+      if ($(this).offset().top - 400 < fromTop)
+        return this;
+    });
+    cur = cur[cur.length-1];
+    var id = cur && cur.length ? cur[0].id : "";
+    
+    if (lastId !== id) {
+        lastId = id;
+        menuItems
+          .parent().removeClass("active")
+          .end().filter(function(index) {
+                 return $(this).attr("href") === "#" + id;
+               }).parent().addClass("active");
+    }                   
+ });
+
+ function download() {
+    const a = document.createElement('a')
+    a.href = "#download";
+    document.body.appendChild(a)
+    a.click()
+    document.body.removeChild(a)
+}
